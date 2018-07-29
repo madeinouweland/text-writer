5ba20af550a7af11cc58f9f7f51f6f9e4d2ad3a6
hauptstadtcoder <hauptstadtcoder@fastmail.com>
2018-07-29 21:50:58 +0200

    save text.txt test file

--- saver.js
+++ saver.js
@@ -1,3 +1,13 @@
 function save(dbx, text) {
-  console.log(dbx, text);
+  console.log(text);
+
+  var file = text;
+
+  dbx.filesUpload({path: '/tostie.txt', contents: file, mode: 'overwrite'})
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
 }
