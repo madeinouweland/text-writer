a5c15155db54495e38dfcb105f3c21852edd5cc4
Hauptstadtcoder <hauptstadtcoder@fastmail.com>
2019-05-07 23:10:14 +0200

    cleanup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function getDropbox() {
-  var token = window.localStorage.getItem("token");
-    if (token) {
-      return dbx = new Dropbox.Dropbox({ accessToken: token });
-    } else {
-      window.location.href='requestaccess.html';
-    };
-}
